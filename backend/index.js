34cf2eb6bb12275cdd4cfe4fac2ed442ad8db6ba
rramirezsoft <rramirezsoft@gmail.com>
2025-05-01 01:20:42 +0200

    added health route for koyeb

--- backend/index.js
+++ backend/index.js
@@ -71,14 +71,20 @@ app.use('/api/userPlace', userPlaceRoutes);
 // Puerto
 const port = process.env.PORT || 3001;
 
+// Rutas del servidor
+// Ruta de prueba para verificar que el servidor está funcionando
+app.get('/', (req, res) => {
+    res.send('API funcionando correctamente');
+});
+
+// Health check para Koyeb
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 // Iniciamos el servidor
 app.listen(port, () => {
-    console.log("Servidor escuchando en el puerto " + port)
-    // pintamos en el navegador un texto para verificar que el servidor esta corriendo
-    app.get('/', (req, res) => {
-        res.send('API funcionando correctamente')
-    })
-
-})
+    console.log("Servidor escuchando en el puerto " + port);
+});
 
 module.exports = app;
